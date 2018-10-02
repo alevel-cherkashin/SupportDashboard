a48892314836a36884d76df34ac6ea49eb820a3a
Vitalii <job.corsar@gmail.com>
2018-10-02 21:23:04 +0300

    Create API Controller

---
+++ SupportDashboard.BusinessLogic/Service/AppService.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace SupportDashboard.BusinessLogic.Service
+{
+    public abstract class AppService<T>
+    {
+        protected string _file;
+        protected List<T> _items;
+
+        public abstract void Add(T item);
+        public abstract void Update(T item);
+        public abstract void Delete(int id);
+
+        public abstract T Get(int id);
+        protected abstract int GetMaxId();
+
+        public List<T> GetAll()
+        {
+            return _items;
+        }
+
+        protected void _save()
+        {
+            string items = JsonConvert.SerializeObject(_items);
+            File.WriteAllText(_file, items);
+        }
+    }
+}
